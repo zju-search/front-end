2e1435401a8e68a632eab576ffbbdf2c714fe979
iwzy7071 <iwzy7071@gmail.com>
2020-06-10 20:48:34 +0800

    1.修复注册功能 2.完成部分可视化功能

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,25 +1,20 @@
 <template>
     <a-form :form="form" :label-col="{ span: 3 }" :wrapper-col="{ span: 12 }">
-        <a-form-item label="用户名">
-            <a-input
-                    v-decorator="['name', { rules: [{ required: true, message: '请输入用户名' },{type:'string',min:6,trigger:'blur'}] }]"/>
+        <a-form-item label="用户名" has-feedback="">
+            <a-input v-decorator="['name', { rules: [{ required: true, message: '请输入用户名' },{type:'string',min:6,message: '用户名至少为6位'}] }]"/>
         </a-form-item>
-        <a-form-item label="邮箱">
-            <a-input
-                    v-decorator="['email', { rules: [{ required: true, message: '请输入用户邮箱'},{validator: this.checkEmail}] }]"/>
+        <a-form-item label="邮箱" has-feedback="">
+            <a-input v-decorator="['email', { rules: [{ required: true, message: '请输入用户邮箱'},{validator: this.checkEmail}] }]"/>
         </a-form-item>
-        <a-form-item label="密码">
-            <a-input type="password" v-decorator="['password', { rules: [{ required: true, message: '请输入用户密码' },
-            {type:'string',min:6}]}]"/>
+        <a-form-item label="密码" has-feedback="">
+            <a-input type="password" v-decorator="['password', { rules: [{ required: true, message: '请输入用户密码' },{validator: handlePass},
+            {type:'string',min:6,message:'密码至少为6位'}]}]" name="password"/>
         </a-form-item>
-        <a-form-item label="验证密码">
-            <a-input type="password" v-decorator="['confirm_password', { rules: [{ required: true, message: '请再次输入密码' },
-            {type:'string',min:6}]}]"/>
+        <a-form-item label="验证密码" has-feedback>
+            <a-input type="password" v-decorator="['confirm_password',{rules: [{validator: handleConfirmPass}]}]" name="confirm_password"/>
         </a-form-item>
         <a-form-item :wrapper-col="{ span: 5, offset: 2 }">
-            <a-button type="primary" html-type="submit" @click="handleSubmit">
-                注册
-            </a-button>
+            <a-button type="primary" html-type="submit" @click="handleSubmit">注册</a-button>
         </a-form-item>
     </a-form>
 </template>
@@ -61,6 +56,16 @@
                     callback(new Error('Email输入格式有误'))
                 }
                 callback()
+            },
+            handlePass(rule, value, callback) {
+                this.password = value;
+                callback();
+            },
+            handleConfirmPass(rule, value, callback) {
+                if (this.password && this.password !== value) {
+                    callback('两次密码输入不一致！');
+                }
+                callback();
             },
         },
     }
--- src/components/Vision.vue
+++ src/components/Vision.vue
@@ -12,50 +12,69 @@
             <a-statistic title="问卷发放总数" :value="number"/>
             <a-statistic title="问卷类型" :value="type"/>
         </div>
+        <div>
+            <div class="ant-statistic-title">填写地域分布</div>
+            <div style="min-height: 500px; justify-content: center;position: relative" id="map"/>
+        </div>
+        <div v-for="(question,index) in questions" :key="index">
+            <div class="ant-statistic-title">{{index+1}}:{{question}}</div>
+            <div v-if></div>
+        </div>
     </div>
 </template>
 
 <script>
-    import { Scene, HeatmapLayer } from '@antv/l7';
-    import { Mapbox } from '@antv/l7-maps';
-    const scene = new Scene({
-        id: 'map',
-        map: new Mapbox({
-            style: 'dark',
-            pitch: 0,
-            center: [ 127.5671666579043, 7.445038892195569 ],
-            zoom: 2.632456779444394
-        })
-    });
-    scene.on('loaded', () => {
-        fetch(
-            'https://gw.alipayobjects.com/os/basement_prod/d3564b06-670f-46ea-8edb-842f7010a7c6.json'
-        )
-            .then(res => res.json())
-            .then(data => {
-                const layer = new HeatmapLayer({})
-                    .source(data)
-                    .shape('heatmap')
-                    .size('mag', [ 0, 1.0 ]) // weight映射通道
-                    .style({
-                        intensity: 2,
-                        radius: 20,
-                        opacity: 1.0,
-                        rampColors: {
-                            colors: [
-                                '#FF4818',
-                                '#F7B74A',
-                                '#FFF598',
-                                '#91EABC',
-                                '#2EA9A1',
-                                '#206C7C'
-                            ].reverse(),
-                            positions: [ 0, 0.2, 0.4, 0.6, 0.8, 1.0 ]
-                        }
-                    });
-                scene.addLayer(layer);
-            });
-    });
+    import {Scene, HeatmapLayer} from '@antv/l7';
+    import {Mapbox} from '@antv/l7-maps';
+
+    window.onLoad = function () {
+        const scene = new Scene({
+            id: 'map',
+            map: new Mapbox({
+                style: 'dark',
+                pitch: 0,
+                center: [127.5671666579043, 7.445038892195569],
+                zoom: 2.632456779444394
+            })
+        });
+
+        scene.on('loaded', () => {
+            fetch(
+                'https://gw.alipayobjects.com/os/basement_prod/d3564b06-670f-46ea-8edb-842f7010a7c6.json'
+            )
+                .then(res => res.json())
+                .then(data => {
+                    const layer = new HeatmapLayer({})
+                        .source(data)
+                        .shape('heatmap')
+                        .size('mag', [0, 1.0]) // weight映射通道
+                        .style({
+                            intensity: 2,
+                            radius: 20,
+                            opacity: 1.0,
+                            rampColors: {
+                                colors: [
+                                    '#FF4818',
+                                    '#F7B74A',
+                                    '#FFF598',
+                                    '#91EABC',
+                                    '#2EA9A1',
+                                    '#206C7C'
+                                ].reverse(),
+                                positions: [0, 0.2, 0.4, 0.6, 0.8, 1.0]
+                            }
+                        });
+                    scene.addLayer(layer);
+                });
+        });
+    };
+
+    const url = 'https://webapi.amap.com/maps?v=1.4.15&key=15cd8a57710d40c9b7c0e3cc120f1200&callback=onLoad';
+    const jsapi = document.createElement('script');
+    jsapi.charset = 'utf-8';
+    jsapi.src = url;
+    document.head.appendChild(jsapi);
+
     export default {
         data() {
             return {
@@ -67,9 +86,10 @@
                 questions: [],
                 answers: [],
                 title: "123213",
-                type: "1"
+                type: "1",
             };
         },
+
         mounted() {
             let formData = new FormData();
             let id = this.$route.query.id;
@@ -80,25 +100,26 @@
                 _this.deadTime = data.deadTime;
                 _this.createTime = data.createTime;
                 _this.fillNumber = data.fillNumber;
-                if(data.number > 0){
+                if (data.number > 0) {
                     _this.number = data.number;
-                }else{
+                } else {
                     _this.number = "无限制"
                 }
                 _this.questionTypes = data.questionTypes;
                 _this.questions = data.questions;
                 _this.answers = data.answers;
                 _this.title = data.title;
-                if(data.type === 0){
+                if (data.type === 0) {
                     _this.type = "只限注册用户";
-                }else if(data.type === 1){
+                } else if (data.type === 1) {
                     _this.type = "累积达到一定数量截止";
-                }else if(data.type == 2){
+                } else if (data.type == 2) {
                     _this.type = "每日只允许填写一定数量"
-                }else {
+                } else {
                     _this.type = "无限制问卷类型";
                 }
-            })
+                _this.n = _this.questions.length;
+            });
         },
     };
 </script>
