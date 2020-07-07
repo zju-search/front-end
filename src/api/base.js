950c8ba183b9acab7fa556006c8ea8edb6430419
Xu Zhang <36254751+clickmouse@users.noreply.github.com>
2020-07-07 14:36:28 +0800

    zhangxv

---
+++ src/api/base.js
@@ -0,0 +1,12 @@
+const base = {
+    //行情中心
+    mc: 'http://39.101.216.58:3000/mock/11/api',
+    //筛选器
+    fi: 'http://39.101.216.58:3000/mock/11/api',
+    //股票详情
+    sd: 'http://39.101.216.58:3000/mock/11/api',
+    //分类查找
+    cs: 'http://39.101.216.58:3000/mock/11/api'
+}
+
+export default base;
---
+++ src/api/fi.js
@@ -0,0 +1,27 @@
+
+import base from './base'
+import axios from '@/utils/http'
+
+
+const Filter = {
+    //排行榜
+    Rank(params) {
+        return axios.get(`${base.fi}/stockRanking`,{
+            params: params
+        })
+    },
+    //条件
+    Condition(params) {
+        return axios.get(`${base.fi}/condition`,{
+            params: params
+        })
+    },
+    //筛选器
+    Screen(params){
+        return axios.get(`${base.fi}/screener`,{
+            params: params
+        })
+    }
+}
+
+export default Filter
