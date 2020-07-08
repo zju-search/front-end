e48fe5920b30cb8de77827ae3fa92d65c9e470f3
patrick <patrickrichard@163.com>
2020-07-08 10:02:11 +0800

    第一轮迭代

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import Ads from 'vue-google-adsense'
 import axios from 'axios'
+import api from './api' // 导入api接口
+
+Vue.prototype.$api = api; // 将api挂载到vue的原型上
 
 Vue.use(require('./mock'));
 Vue.config.productionTip = false;
@@ -19,7 +22,10 @@ Vue.use(Ads.InFeedAdsense);
 
 
 new Vue({
+    el: '#app',
+    components: {app : App},
     router,
     store,
-    render: h => h(App)
-}).$mount('#app');
+    render: h => h('app')
+}).$mount("#app");
+
